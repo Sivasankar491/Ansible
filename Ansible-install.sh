7366644723fb7f4d1f3995b02061433dffa76be1
Sankar491 <sivasankarrcks@gmail.com>
2024-12-13 22:27:14 +0530

    Ansible

---
+++ Ansible-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT() {
+    if [ $USER_ID -ne 0 ]
+    then    
+        echo -e "$R Please run the script with root user$N"
+        exit 1
+    fi
+}
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R FAILED $N"
+    else
+        echo -e "$2 is $G SUCCESS $N"
+    fi
+}
+CHECK_ROOT
+
+sudo dnf installed list|grep ansible
+
+if [ $? -ne 0 ]
+then    
+    echo -e "Ansible is not installed yet, $Y installing now $N"
+    dnf install ansible -y
+    VALIDATE $? "Installing Ansible"
+else
+    echo -e "Ansible  is already installed.. $Y SKIPPING $N"
+fi
